001e096013703528ee982049e8b3613651ef713b
Bruno Sobral <brunosobralss@hotmail.com>
2022-05-16 17:24:57 -0300

    test csv files read

--- reactjs/hooks/naruto-server/src/routes.ts
+++ reactjs/hooks/naruto-server/src/routes.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import readline from 'readline';
 import * as csv from 'fast-csv';
 
+
 const router = Router();
 
 const quotes = {
@@ -12,20 +13,29 @@ const quotes = {
 };
 
 router.get('/', (req: Request, res: Response) => {
+  function getRandomInt(min: any, max: any) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  let indiceQuoteRandom = getRandomInt(0, 3);
+  console.log(indiceQuoteRandom)
+
   const readable = fs.createReadStream("./produtos.csv")
   .pipe(csv.parse())
   .on("error", (error: any) => console.log("read finished"))
-  .on("data", (data: any) => res.send(
-    data
+  .on("data", (data: any) => res.json(
+    data[indiceQuoteRandom]
   ));
 
-  // const streamCsv = csv().on('data', (data: any) => console.log(data))
-  // readable.pipe(streamCsv.parse())
-  
 })
 
 router.get('/quotes', (req: Request, res: Response) => {
   res.json(quotes)
+})
+
+router.get('/test', (req: Request, res: Response) => {
 })
 
 export { router };
